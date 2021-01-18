f15a9180de9e5e7edd17627b9d197e299156e5ee
bits vardhan <gsshaurya@gmail.com>
2021-01-18 15:34:15 +0530

    firebase added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import { AuthProvider } from '../lib/auth';
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
 }
 
 export default MyApp
