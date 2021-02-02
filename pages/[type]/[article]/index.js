22b4c551e038255df141bf5fe501b2de910fafb3
Animesh-chaturvedi <animesh@mudrex.com>
2021-02-02 22:14:02 +0530

    new start

---
+++ pages/[type]/[article]/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ArticleComponent from "../../../components/ArticleComponent/ArticleComponent"
+export default function index() {
+    return (
+        <div>
+            <ArticleComponent />
+        </div>
+    )
+}
