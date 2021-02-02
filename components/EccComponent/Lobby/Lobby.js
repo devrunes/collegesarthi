f99f5e2107bafbcd315b7614db82098a7c25686d
Piyush Bhargav <piyush.bhargav70@gmail.com>
2021-02-02 22:21:49 +0530

    lobby update

--- components/EccComponent/Lobby/Lobby.js
+++ components/EccComponent/Lobby/Lobby.js
@@ -1,11 +1,19 @@
 import React from "react";
+import styles from "./Lobby.module.css";
 import HomeCard from "../../HomeCard/HomeCard";
 
 const Lobby = ({ type }) => {
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
-    <div>
+    <div className={styles.lobbyWrapper}>
       <div>this is lobby of type {type}</div>
-      <HomeCard/>
+      <div className={styles.lobbyCardComp}>
+        {data.map((card) => (
+          <div className={styles.lobby}>
+            <HomeCard title="Jee Mains" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
