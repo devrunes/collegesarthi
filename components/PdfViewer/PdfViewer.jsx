121c9a6515411feaa757a0ed9e0e037a0acd3837
Shaurya Vardhan <gsshaurya@gmail.com>
2021-03-26 01:18:22 +0530

    feat: add pdf viewer

---
+++ components/PdfViewer/PdfViewer.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+export default function PdfViewer() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div>
+      <Document file="/yasin.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </div>
+  );
+}
---
+++ pages/showpdf.js
@@ -0,0 +1,11 @@
+import dynamic from "next/dynamic";
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import("../components/PdfViewer/PdfViewer"),
+  {
+    ssr: false,
+  }
+);
+
+const comp = () => <DynamicComponentWithNoSSR />;
+export default comp;
