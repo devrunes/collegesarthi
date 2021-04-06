e22273e02966582f7e5695d19d2bcba94f7ff3a1
Shaurya Vardhan <gsshaurya@gmail.com>
2021-04-06 20:40:09 +0530

    feat: adds multiple pages of pdf

--- components/PdfViewer/PdfViewer.jsx
+++ components/PdfViewer/PdfViewer.jsx
@@ -1,23 +1,81 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import firebase from "firebase";
 
 export default function PdfViewer() {
+  // const [numPages, setNumPages] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
+
+  // function onDocumentLoadSuccess({ numPages }) {
+  //   setNumPages(numPages);
+  // }
+  // const storage = firebase.storage();
+  // storage
+  //   .refFromURL(
+  //     "gs://collegesarthi-54ff1.appspot.com/Papers/CAT/CAT-2017-Question-Paper-with-Solution-SLOT-1.pdf"
+  //   )
+  //   .getDownloadURL()
+  //   .then((res) => console.log(res));
+  // // console.log();
+
+  // return (
+  //   <div>
+  //     <Document
+  //       file="https://firebasestorage.googleapis.com/v0/b/collegesarthi-54ff1.appspot.com/o/Papers%2FCAT%2FCAT-2017-Question-Paper-with-Solution-SLOT-1.pdf?alt=media&token=2ef48bb9-8465-4a32-b826-79db684c587d"
+  //       onLoadSuccess={onDocumentLoadSuccess}
+  //     >
+  //       <Page pageNumber={pageNumber} />
+  //     </Document>
+  //     <p>
+  //       Page {pageNumber} of {numPages}
+  //     </p>
+  //   </div>
+  // );
+
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
   }
 
   return (
-    <div>
-      <Document file="/yasin.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+    <>
+      <Document
+        file="https://firebasestorage.googleapis.com/v0/b/collegesarthi-54ff1.appspot.com/o/Papers%2FCAT%2FCAT-2017-Question-Paper-with-Solution-SLOT-1.pdf?alt=media&token=2ef48bb9-8465-4a32-b826-79db684c587d"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
         <Page pageNumber={pageNumber} />
       </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-    </div>
+      <div>
+        <p>
+          Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+        </p>
+        <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+          Previous
+        </button>
+        <button
+          type="button"
+          disabled={pageNumber >= numPages}
+          onClick={nextPage}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 }
